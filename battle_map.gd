5a4a5cfc15b487856a5306a87a6b1b68962da4f3
Tyler Kirchner <49655028+KirchnerT@users.noreply.github.com>
2025-05-29 12:51:02 -0500

    Fix bug

--- battle_map.gd
+++ battle_map.gd
@@ -5,7 +5,7 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	print(AllyArmy.units)
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
