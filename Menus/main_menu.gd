7730d58c959ed9cd0ed91fbf4385e9ca9bcd6aad
FLYNNREAP3R <111404122+FLYNNREAP3R@users.noreply.github.com>
2025-06-03 07:59:19 -0400

    menu... GOING

---
+++ Menus/main_menu.gd
@@ -0,0 +1,19 @@
+extends Control
+
+
+
+
+func _on_play_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/battle_map.tscn") # Replace with function body.
+
+
+func _on_decks_pressed() -> void:
+	get_tree().change_scene_to_file("res://Menus/decks_menu.tscn") # Replace with function body.
+
+
+func _on_options_pressed() -> void:
+	get_tree().change_scene_to_file("res://Menus/options_menu.tscn") # Replace with function body.
+
+
+func _on_quit_pressed() -> void:
+	get_tree().quit() # Replace with function body.
