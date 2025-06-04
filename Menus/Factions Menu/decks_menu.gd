1d1996f3cbb69a348de6edffe906b46991408c85
FLYNNREAP3R <111404122+FLYNNREAP3R@users.noreply.github.com>
2025-06-04 07:46:55 -0400

    menu... EVEN MORE SHIT

--- Menus/Factions Menu/decks_menu.gd
+++ Menus/Factions Menu/decks_menu.gd
@@ -1,6 +1,6 @@
 extends Control
+#Replace with function body.
 
 
-
-func _on_back_pressed() -> void:
-	get_tree().change_scene_to_file("res://Menus/main_menu.tscn") # Replace with function body.
+func _on_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://Menus/Main Menu/main_menu.tscn") # Replace with function body.
--- Menus/Options Menu/audio_slider_settings.gd
+++ Menus/Options Menu/audio_slider_settings.gd
@@ -4,6 +4,29 @@ extends Control
 @onready var h_slider: HSlider = $HBoxContainer/HSlider as HSlider
 
 
-@export_enum("Master", "Music", "SFX") var bus_name : string
+@export_enum("Master", "MUSIC", "SFX") var bus_name : String
 
 var bus_index: int = 0
+
+func _ready():
+	h_slider.value_changed.connect(on_value_changed)
+	get_bus_name_by_index()
+	set_name_label_text()
+	set_slider_value()
+
+# Sets the name of the label depending on the bus	
+func set_name_label_text() -> void:
+	audio_name_label.text = str(bus_name).to_upper() + " VOLUME"
+	
+# Grabs the bus name from the Audio Server
+func get_bus_name_by_index() -> void:
+	bus_index = AudioServer.get_bus_index(bus_name)
+
+# Sets the initial slider value 
+func set_slider_value() -> void:
+	h_slider.value = db_to_linear(AudioServer.get_bus_volume_db(bus_index))
+
+# Updates the bus volume based on what bus is being adjusted
+func on_value_changed(value: float):
+	AudioServer.set_bus_volume_db(bus_index, linear_to_db(value))
+	
